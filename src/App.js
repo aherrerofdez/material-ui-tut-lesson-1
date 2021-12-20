897ead4da687e6b77dda9bfeba76973bfc74cbd3
aherrerofdez <aherrero.mis2016@student.ie.edu>
2021-12-20 17:11:26 +0100

    initializing the app and setting appbar

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Booking from './pages/Booking'
+import Availability from './pages/Availability'
+import Contact from './pages/Contact'
+import Profile from './pages/Profile'
+import Reservations from './pages/Reservations'
+import Logout from './pages/Logout'
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/booking">
+          <Booking />
+        </Route>
+        <Route path="/availability">
+          <Availability />
+        </Route>
+        <Route path="/contact-us">
+          <Contact />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/reservations">
+          <Reservations />
+        </Route>
+        <Route path="/logout">
+          <Logout />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
