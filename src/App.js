84d58f8604321020f6696ef4d6957a3fa0137a01
aherrerofdez <aherrero.mis2016@student.ie.edu>
2022-01-04 23:54:46 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Booking from './pages/Booking'
+import Spaces from './pages/Spaces'
+import Contact from './pages/Contact'
+import Profile from './pages/Profile'
+import Reservations from './pages/Reservations'
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/spaces">
+          <Spaces />
+        </Route>
+        <Route path="/booking">
+          <Booking />
+        </Route>
+        <Route path="/contact-us">
+          <Contact />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/my-reservations">
+          <Reservations />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
