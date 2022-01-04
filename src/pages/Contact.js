af4de44b27eb8d8f3c9bd5fd4c9b9802aa337481
elenamolinariii <50439082+elenamolinariii@users.noreply.github.com>
2022-01-04 10:25:44 +0100

    Update Contact.js
    - commented out the dialog box because it was not behaving properly
    - added an "alert" to confirm receiving the message
    - the inputted info is logged on the console
    - added links to social media platforms
    - added aria labels to all buttons that are links or that perform actions (it didn't show any info otherwise, especially for the links to social media)

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,23 +6,31 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
+import { useForm } from "react-hook-form";
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+{/*
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import { useForm } from "react-hook-form";
-import { waitFor } from '@testing-library/react';
+import DialogTitle from '@mui/material/DialogTitle'; 
+*/}
 
 export default function Contact() {
+  
   {/*data registration and validation*/}
   const { register, handleSubmit, formState: { errors } } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
+    alert('We received your message! We will get back to you soon!')
   }
 
-  {/*dialog to confirm submission of message*/}
+  {/*dialog to confirm submission of message (not working)
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -33,6 +41,7 @@ export default function Contact() {
     setOpen(false);
     window.location.reload(true);
   };
+  */}
 
   return (
     <>
@@ -40,11 +49,15 @@ export default function Contact() {
       <Box 
         component="form"
         sx={{ '& .MuiTextField-root': {marginTop: 2 }, flexGrow: 1 }}
-        onSubmit={handleSubmit(onSubmit), handleClickOpen}
+        onSubmit={onSubmit}
         >
         
         <Typography variant='h5' sx = {{paddingLeft: 5, paddingTop: 5, paddingBottom: 2, fontWeight: 'bold', fontFamily: 'montserrat'}}>
             Contact us
+        </Typography>
+
+        <Typography variant='h6' sx = {{paddingLeft: 5, paddingBottom: 2, fontWeight: 'bold'}}>
+            Send us a message: 
         </Typography>
 
         <Grid container spacing={0}>
@@ -100,34 +113,76 @@ export default function Contact() {
           </Grid>
 
           <Grid item xs={12} sx = {{marginLeft: 5, marginTop: 2, marginRight: 5}}>
-            <Button type = 'submit' variant="contained" endIcon={<SendIcon />}>
+            <Button 
+              type = 'submit' 
+              variant="contained" 
+              endIcon={<SendIcon />}
+              aria-label='Send Message'>
               Send
+            </Button>
+          </Grid>
+
+          <Grid item xs={12} sx = {{marginLeft: 5, marginTop: 2, marginRight: 5}}>
+            <Typography variant='h6' sx = {{marginBottom: 2, marginTop: 5, fontWeight: 'bold'}}>
+              Find us on social media: 
+            </Typography>
+          </Grid>
+
+          <Grid item xs={12} sx = {{marginLeft: 5, marginRight: 5, marginBottom: 5}}>
+            <Button 
+                href="https://www.instagram.com/accounts/login/"
+                aria-label='Instagram'
+            >
+              <InstagramIcon fontSize="large"/>
+            </Button>
+
+            <Button 
+                href="https://m.facebook.com/home.php"
+                aria-label='Facebook'
+            >
+              <FacebookIcon fontSize="large" />
+            </Button>
+
+            <Button 
+                href="https://www.linkedin.com/"
+                aria-label='LinkedIn'
+            >
+              <LinkedInIcon fontSize="large" />
+            </Button>
+
+            <Button 
+                href="https://mobile.twitter.com/login"
+                aria-label='Twitter'
+            >
+              <TwitterIcon fontSize="large" />
             </Button>
           </Grid>
 
         </Grid>
       </Box>
       
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"We received your message!"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            We will get back to you very soon to make sure we solve all your doubts!
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} autoFocus>
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
+      {/* dialog to confirm receiving message (not working)
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"We received your message!"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              We will get back to you very soon to make sure we solve all your doubts!
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} autoFocus>
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
+      */}
       
     </>
   )
