7202009fdae5a964b96b4b8042973837965de2e5
aherrerofdez <aherrero.mis2016@student.ie.edu>
2022-01-05 03:35:20 +0100

    Update Reservations.js

--- src/pages/Reservations.js
+++ src/pages/Reservations.js
@@ -1,58 +1,118 @@
+import * as React from 'react'
 import Header from '../components/Header'
-import Typography from '@mui/material/Typography';
-import { DataGrid } from '@mui/x-data-grid'
+import PropTypes from 'prop-types'
+import Box from '@mui/material/Box'
+import Collapse from '@mui/material/Collapse'
+import IconButton from '@mui/material/IconButton'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Typography from '@mui/material/Typography'
+import Paper from '@mui/material/Paper'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.getValue(params.id, 'firstName') || ''} ${
-        params.getValue(params.id, 'lastName') || ''
-      }`,
-  },
-];
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row"> {row.name} </TableCell>
+        <TableCell> {row.space} </TableCell>
+        <TableCell> {row.date} </TableCell>
+      </TableRow>
+
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                Reservation Details
+              </Typography>
+
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell> Customer </TableCell>
+                    <TableCell> Time </TableCell>
+                    <TableCell> Duration </TableCell>
+                    <TableCell align="right"> Total price (€) </TableCell>
+                  </TableRow>
+                </TableHead>
+
+                <TableBody>
+                  {row.details.map((detailsRow) => (
+                    <TableRow key={detailsRow.customerId}>
+                      <TableCell component="th" scope="row"> {detailsRow.customerId} </TableCell>
+                      <TableCell> {detailsRow.time} </TableCell>
+                      <TableCell> {detailsRow.duration} </TableCell>
+                      <TableCell align="right"> {detailsRow.price} </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+Row.propTypes = {
+  row: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    space: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    details: PropTypes.arrayOf(
+      PropTypes.shape({
+        customerId: PropTypes.number.isRequired,
+        time: PropTypes.string.isRequired,
+        duration: PropTypes.string.isRequired,
+        price: PropTypes.string.isRequired
+      })).isRequired})
+  .isRequired};
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+  {name: 'Team Meeting', space: 'Big Blue', date: '12-01-2022', details:
+    [{customerId: '11091700', time: '10:30', duration: '1 hour', price: '25€'}]},
+  {name: 'Meeting with VC', space: 'Big Meeting Room', date: '14-01-2022', details:
+  [{customerId: '11091700', time: '12:00', duration: '30 min', price: '45€'}]},
+  {name: 'Call with Developer', space: 'Small Meeting Room', date: '17-01-2022', details:
+    [{customerId: '11091700', time: '11:15', duration: '1 hour', price: '65€'}]}];
 
 export default function Reservations() {
+
   return (
     <>
       <Header />
-      <Typography>
-        My reservations
-      </Typography>
-      <div style={{ height: 400, width: '100%' }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          pageSize={5}
-          rowsPerPageOptions={[5]}
-          checkboxSelection
-        />
-      </div>
+      <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell> Reservation ID </TableCell>
+            <TableCell> Space </TableCell>
+            <TableCell> Date </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <Row key={row.name} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     </>
   )
 }
